526f0575e4519b0154fcd88714a3fd378a530082
Sven Willenbuecher <sven.willenbuecher@gmx.de>
2025-06-10 09:48:49 +0200

    PM_FILTER is discouraged
    This reverts commit 7a386a0da7af09c3a276d7003c6b74a5c814f552.

--- lib/Local/New/Module.pm
+++ lib/Local/New/Module.pm
@@ -7,6 +7,9 @@ BEGIN { die 'Unsupported Perl version, stopped' if $] < 5.006_002 };    # uncove
 
 package Local::New::Module;
 
-our $VERSION = '0.0.0';
+# keeping the following $VERSION declaration on a single line is important
+#<<<
+use version 0.9915 qw( ); our $VERSION = version->declare( '1.0.0' );
+#>>>
 
 1
